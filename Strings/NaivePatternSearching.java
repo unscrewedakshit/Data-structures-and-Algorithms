0b53da8376aef43f91f71a9160768440ae412fb9
AKSHIT PANDAY <akshitpanday791@outlook.com>
2021-03-30 15:20:04 +0530

    Update NaivePatternSearching.java

--- Strings/NaivePatternSearching.java
+++ Strings/NaivePatternSearching.java
@@ -1,12 +1,15 @@
-package string;
+package String;
+//Given a text and a pattern . We need to  find index/s(starting) in the text where the pattern exist.
+//eg: text="abababcd", pattern="abab" => Output: 0 2
+//eg: text="abcabcd", pattern="abcd" => Output: 3
+//eg: text="aaaaa", pattern="aaa" => Output: 0 1 2
 
 public class NaivePatternSearching {
     public static void main(String[] args) {
-        String str = "geeksforgeeks";
-        String pattern = "eks";
+        String str = "abababcd";
+        String pattern = "abab";
         patternSearch(str,pattern);
     }
-
 
     private static void patternSearch(String text, String pattern) { //Time Complexity: O(n*m)
         int n = text.length(), m = pattern.length();
@@ -22,6 +25,7 @@ public class NaivePatternSearching {
         }
     }
 
+    //Alternate way
     /*private static void patternSearch(String str, String pattern) {
         int l = pattern.length();
         for(int i=0; i<=str.length() - l; i++) {
